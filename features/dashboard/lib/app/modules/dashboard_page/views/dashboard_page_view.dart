687c0a1ae6246e5f298e98b1d7bf3cc759c44b1b
Julia Stahl <julia.stahl@kepha.com.br>
2023-01-18 11:29:34 -0300

    Pacote dashboard importa Pacote perfil via path

--- features/dashboard/lib/app/modules/dashboard_page/views/dashboard_page_view.dart
+++ features/dashboard/lib/app/modules/dashboard_page/views/dashboard_page_view.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:perfil/app/routes/app_pages.dart';
 
 import '../controllers/dashboard_page_controller.dart';
 
 class DashboardPageView extends GetView<DashboardPageController> {
   const DashboardPageView({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,9 +17,20 @@ class DashboardPageView extends GetView<DashboardPageController> {
         backgroundColor: Colors.purple,
       ),
       body: Center(
-        child: Text(
-          'Bem-vindo a Home',
-          style: TextStyle(fontSize: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              'Bem-vindo a Home',
+              style: TextStyle(fontSize: 20),
+            ),
+            SizedBox(height: 30),
+            ElevatedButton(
+              onPressed: () => Get.toNamed(Routes.PERFIL_PAGE),
+              child: Text('Ir para perfil'),
+            ),
+          ],
         ),
       ),
     );
--- features/dashboard/lib/app/routes/app_pages.dart
+++ features/dashboard/lib/app/routes/app_pages.dart
@@ -1,6 +1,9 @@
 import 'package:dashboard/app/modules/dashboard_page/bindings/dashboard_page_binding.dart';
 import 'package:dashboard/app/modules/dashboard_page/views/dashboard_page_view.dart';
 import 'package:get/get.dart';
+import 'package:perfil/app/modules/perfil_page/bindings/perfil_page_binding.dart';
+import 'package:perfil/app/modules/perfil_page/views/perfil_page_view.dart';
+import 'package:perfil/app/routes/app_pages.dart' as perfil_routes;
 
 part 'app_routes.dart';
 
@@ -9,11 +12,16 @@ class AppPages {
 
   static const INITIAL = Routes.DASHBOARD_PAGE;
 
-  static final routes = [
+  static final routes = <GetPage>[
     GetPage(
       name: _Paths.DASHBOARD_PAGE,
       page: () => const DashboardPageView(),
       binding: DashboardPageBinding(),
+    ),
+    GetPage(
+      name: perfil_routes.Routes.PERFIL_PAGE,
+      page: () => const PerfilPageView(),
+      binding: PerfilPageBinding(),
     ),
   ];
 }
